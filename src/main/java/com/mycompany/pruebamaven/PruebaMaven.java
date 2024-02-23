ae668cc97ca41e59a7b5457023094ed480400adf
ciccx <claudia.chamx@gmail.com>
2024-02-23 14:49:38 -0600

    ejemplo uso de netbeans con git

---
+++ src/main/java/com/mycompany/pruebamaven/PruebaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebamaven;
+
+/**
+ *
+ * @author Admin
+ */
+public class PruebaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
